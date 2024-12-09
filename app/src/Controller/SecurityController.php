26b72924b2f3eddfa1239b29adcf5023504aef0d
Justyna Jawniak <jawniakjustyna@gmail.com>
2024-12-09 02:46:59 +0100

    docs: add PhpDocs for security controller

--- app/src/Controller/SecurityController.php
+++ app/src/Controller/SecurityController.php
@@ -1,5 +1,7 @@
 <?php
-
+/**
+ * Security controller.
+ */
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -7,8 +9,18 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
+/**
+ * Class SecurityController.
+ */
 class SecurityController extends AbstractController
 {
+    /**
+     * Login action.
+     *
+     * @param AuthenticationUtils $authenticationUtils Authentication utilities
+     *
+     * @return Response HTTP Response
+     */
     #[Route(path: '/login', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
@@ -24,6 +36,9 @@ class SecurityController extends AbstractController
         ]);
     }
 
+    /**
+     * Logout action.
+     */
     #[Route(path: '/logout', name: 'app_logout')]
     public function logout(): void
     {
